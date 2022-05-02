65f8ed0f9a2aa0ad444ef0e85573ec2c3c7bdc0f
«Aleksander <pavel4uk18@gmail.com>
2022-05-02 09:24:56 +0300

    Task4Reshenie

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,11 +2,20 @@
 //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.WriteLine("Введите число - >");
-int num = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 
-for(int i=0; i<num.length;i++)
-{
-    if(num[i]%2==0);
-}
-
+ int num = 1;
+            while (num <= a)
+            {
+                int result = num % 2;
+                if (result != 1)
+                {
+                    Console.WriteLine("четное " + num);
+                    num++;
+                }
+                else
+                {
+                    num++;
+                }
+            }
 
